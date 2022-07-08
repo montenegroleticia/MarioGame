b0007900a4eecc3f95854ef5beea71b8a3fea959
Letícia Montenegro <montenegro.letiicia@gmail.com>
2022-07-08 16:43:02 -0300

    Jogo Mario - Base

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@ const loopGame = setInterval(() => {
     mario.style.animation = "none";
     mario.style.bottom = `${marioPosition}px`;
 
-    mario.src = "/assets/img/mario-game-over.png";
+    mario.src = "./assets/img/mario-game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "45px";
 
